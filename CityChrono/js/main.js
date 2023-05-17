f3d6bf220011d8a869dfe64a249c2609bdb85426
dana-66 <88723929+dana-66@users.noreply.github.com>
2023-05-17 21:38:31 +0300

    Update main.js

--- CityChrono/js/main.js
+++ CityChrono/js/main.js
@@ -4,110 +4,19 @@
 
 // start of Create Todo Element function
 function CreateTodoElement(item) {
-    const item_el = document.createElement("div");
-    item_el.classList.add("item");
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.checked = item.complete;
-
-    if (item.complete) {
-        item_el.classList.add("complete");
-    }
-
-    const input_el = document.createElement("input");
-    input_el.type = "text";
-    input_el.value = item.text;
-    input_el.setAttribute("disabled", "");
-
-    const actions_el = document.createElement("div");
-    actions_el.classList.add("actions");
-
-    const edit_btn_el = document.createElement("button");
-    edit_btn_el.classList.add("material-icons");
-    edit_btn_el.innerHTML = '<span class="material-symbols-outlined icon">edit</span>';
-
-    const remove_btn_el = document.createElement("button");
-    remove_btn_el.classList.add("material-icons", "remove-btn");
-    remove_btn_el.innerHTML = '<span class="material-symbols-outlined icon">close</span>';
-
-    actions_el.append(edit_btn_el);
-    actions_el.append(remove_btn_el);
-
-    item_el.append(checkbox);
-    item_el.append(input_el);
-    item_el.append(actions_el);
-
-    // events when creating the elements
-    checkbox.addEventListener("change", () => {
-        item.complete = checkbox.checked;
-
-        if (item.complete) {
-            item_el.classList.add("complete");
-        } else {
-            item_el.classList.remove("complete");
-        }
-
-        save();
-    });
-
-    input_el.addEventListener('input', () => {
-        item.text = input_el.value;
-    });
-
-    input_el.addEventListener("blur", () => {
-        input_el.setAttribute("disabled", "");
-        save();
-    });
-
-    edit_btn_el.addEventListener("click", () => {
-        input_el.removeAttribute("disabled");
-        input_el.focus();
-    });
-
-    remove_btn_el.addEventListener("click", () => {
-        todos = todos.filter(t => t.id != item.id);
-
-        item_el.remove();
-
-        save();
-    });
-
-    return { item_el, input_el, edit_btn_el, remove_btn_el }
-
 }
 // to make the Tasks when refreshing 
 function DisplayTodos() {
-    load();
-
-    for (let i = 0; i < todos.length; i++) {
-        const item = todos[i];
-
-        const { item_el } = CreateTodoElement(item);
-
-        list_el.append(item_el);
-    }
 }
 
 DisplayTodos();
 
 // to remove the removed tasks from the local storage
 function save() {
-    // Remove deleted items from the `todos` array
-    todos = todos.filter(todo => !todo.deleted);
-
-    // Save the updated `todos` array to local storage
-    const save = JSON.stringify(todos);
-    localStorage.setItem("tasks", save);
 }
 
 
 function load() {
-    const data = localStorage.getItem("tasks");
-
-    if (data) {
-        todos = JSON.parse(data);
-    }
 }
 
 const items = document.querySelectorAll('.item');
@@ -244,54 +153,6 @@ function getCurrentAudioElement() {
 // END Radio Div
 
 // START Spotify Div 
-const playlistContainer = document.getElementById("playlist-container");
-const backToMainButton = document.getElementById("back-to-main");
-const showPlaylistButtons = document.querySelectorAll("[id^='show-playlist']");
-
-// Hide all playlists except the first one
-const playlists = playlistContainer.querySelectorAll("iframe");
-for (let i = 0; i < playlists.length; i++) {
-    playlists[i].style.display = "none";
-}
-
-// Show the playlist associated with the pressed button
-function showPlaylist() {
-    // Get the playlist number from the button ID
-    const playlistNumber = parseInt(this.id.slice(-1));
-    // Hide all playlists except the selected one
-    for (let i = 0; i < playlists.length; i++) {
-        if (playlists[i].id === `playlist${playlistNumber}-iframe`) {
-            playlists[i].style.display = "block";
-        } else {
-            playlists[i].style.display = "none";
-        }
-    }
-    // Show the back to main button
-    backToMainButton.style.display = "block";
-    // Hide all show playlist buttons
-    for (let i = 0; i < showPlaylistButtons.length; i++) {
-        showPlaylistButtons[i].style.display = "none";
-    }
-}
-
-// Add event listeners to all show playlist buttons
-for (let i = 0; i < showPlaylistButtons.length; i++) {
-    showPlaylistButtons[i].addEventListener("click", showPlaylist);
-}
-
-// Show all show playlist buttons and hide the back to main button
-function backToMain() {
-    for (let i = 0; i < showPlaylistButtons.length; i++) {
-        showPlaylistButtons[i].style.display = "inline-block";
-    }
-    for (let i = 0; i < playlists.length; i++) {
-        playlists[i].style.display = "none";
-    }
-    backToMainButton.style.display = "none";
-}
-
-// Add event listener to back to main button
-backToMainButton.addEventListener("click", backToMain);
 
 // END Spotify Div
 
